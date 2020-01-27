37314c546b62cf25179f51136da5cde7e19ca90a
G. Michael Fitzgerald <38142372+tic@users.noreply.github.com>
2020-01-27 10:07:02 -0500

    Switch to macro-defined byte signals

--- serialController.ino
+++ serialController.ino
@@ -1,6 +1,10 @@
 #define BTN_PIN 2
 #define LED_PIN 1
 
+#define BTN_A_DOWN "\x20"
+#define BTN_A_UP "\x21"
+
+
 void setup()
 {
     pinMode(BTN_PIN, INPUT);
@@ -15,12 +19,12 @@ void loop()
     //test comment
     if(!button && digitalRead(BTN_PIN) == HIGH) {
       button = true;
-      Serial.write("button pressed\n");
+      Serial.write(BTN_A_DOWN);
     }
 
     if(button && digitalRead(BTN_PIN) == LOW) {
       button = false;
-      Serial.write("button released\n");
+      Serial.write(BTN_A_UP);
     }
 
     digitalWrite(LED_PIN, button ? HIGH : LOW);
