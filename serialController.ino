093b1c70b8af9c7b8378108f3aae94a6a31e2e39
G. Michael Fitzgerald <38142372+tic@users.noreply.github.com>
2020-02-03 09:57:28 -0500

    Joystick prep code

--- serialController.ino
+++ serialController.ino
@@ -1,9 +1,22 @@
 #define BTN_PIN 2
 #define LED_PIN 1
+#define JOYSTICK_X A0
+#define JOYSTICK_Y A1
+
+#define MIDDLE 512
+#define THRESHOLD 100
+
 
 #define BTN_A_DOWN "\x20"
 #define BTN_A_UP "\x21"
-
+#define JOYSTICK_GO_LF "\x24"
+#define JOYSTICK_LV_LF "\x25"
+#define JOYSTICK_GO_UP "\x26"
+#define JOYSTICK_LV_UP "\x27"
+#define JOYSTICK_GO_RT "\x28"
+#define JOYSTICK_LV_RT "\x29"
+#define JOYSTICK_GO_DN "\x2A"
+#define JOYSTICK_LV_DN "\x2B"
 
 void setup()
 {
@@ -13,19 +26,66 @@ void setup()
 }
 
 bool button = false;
+int xpos = 0;
+int ypos = 0;
+
+int beyondThreshold(int reading) {
+  if(reading > MIDDLE + THRESHOLD) return 1;
+  if(reading < MIDDLE + THRESHOLD) return -1;
+  return 0;
+}
 
 void loop()
 {
-    //test comment
-    if(!button && digitalRead(BTN_PIN) == HIGH) {
-      button = true;
-      Serial.write(BTN_A_DOWN);
+    bool btnA = digitalRead(BTN_PIN);
+    if(btnA != button) { // If they disagree, there will be an update.
+        button = btnA;
+        Serial.write(btnA ? BTN_A_DOWN : BTN_A_UP);
     }
 
-    if(button && digitalRead(BTN_PIN) == LOW) {
-      button = false;
-      Serial.write(BTN_A_UP);
+    int joystickX = beyondThreshold(analogRead(JOYSTICK_X));
+    int joystickY = beyondThreshold(analogRead(JOYSTICK_Y));
+
+
+    // the 8 different joystick axis changes
+    // this uses -1 as left, but -1 would be up on the y axis
+    
+    // -1 -1 --> -1, send "" (nothing)
+    // -1  0 -->  0, send "\x25" (leave left)
+    // -1  1 -->  1, send "\x25\x28" (leave left, enter right)
+    //  0 -1 --> -1, send "\x24" (enter left)
+    //  0  0 -->  0, send "" (nothing)
+    //  0  1 -->  1, send "\x28" (enter right)
+    //  1 -1 --> -1, send "\x29\x24" (leave right, enter left)
+    //  1  0 -->  0, send "\x29" (leave right)
+    //  1  1 -->  1, send "" (nothing)
+
+    String jsBytes = "";
+    if(xpos != joystickX) { // If they disagree, there will be an update.
+      // Resolve "leaving" bytes
+      if(xpos == -1) jsBytes = "\x25";
+      else if(xpos == 1) jsBytes = "\x29";
+
+      // Resolve "entering" bytes
+      if(joystickX == -1) jsBytes += "\x24";
+      else if(joystickX == 1) jsBytes += "\x28";
+
+      xpos = joystickX;
     }
+
+    if(joystickY != ypos) { // If they disagree, there will be an update.
+      // Resolve "leaving" bytes
+      if(ypos == -1) jsBytes += "\x27";
+      else if(ypos == 1) jsBytes += "\x2B";
+
+      // Resolve "entering" bytes
+      if(joystickY == -1) jsBytes += "\x26";
+      else if(joystickY == 1) jsBytes += "\x2A";
+
+      ypos = joystickY;
+    }
+
+    if(jsBytes =! "") Serial.write(jsBytes); // Send joystick commands, if they exist.
 
     digitalWrite(LED_PIN, button ? HIGH : LOW);
     delay(10);
